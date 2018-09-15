df6ea4f286bfb763a492aa190f147fb9510a8345
Bruno C. Couto <brunodev02221@gmail.com>
2018-09-15 09:54:03 -0300

    Change react "Input" component definition
    Make it a functional component.

--- packages/react/src/Input.js
+++ packages/react/src/Input.js
@@ -1,22 +1,17 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import Mask from "./Mask";
 
-class Input extends PureComponent {
-  render() {
-    const { mask, defaultValue, ...rest } = this.props;
-    return (
-      <Mask pattern={mask} defaultValue={defaultValue}>
-        {({ value, handleChange }) => (
-          <input
-            type="text"
-            defaultValue={value}
-            onChange={handleChange}
-            {...rest}
-          />
-        )}
-      </Mask>
-    );
-  }
-}
+const Input = ({ mask, defaultValue, ...rest }) => (
+  <Mask pattern={mask} defaultValue={defaultValue}>
+    {({ value, handleChange }) => (
+      <input
+        type="text"
+        defaultValue={value}
+        onChange={handleChange}
+        {...rest}
+      />
+    )}
+  </Mask>
+);
 
 export default Input;
