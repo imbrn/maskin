66d0af494cae8e806f4d8c4b98b22074f00fb5de
Bruno C. Couto <brunodev02221@gmail.com>
2018-09-29 15:22:54 -0300

    react: Fix reference to `withMasking` into `index.js` file

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,3 +1,3 @@
 export { default as Mask } from "./Mask";
 export { default as Input } from "./Input";
-export { default as withMask } from "./withMask";
+export { default as withMaskin } from "./withMaskin";
